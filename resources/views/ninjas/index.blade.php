a5115877c3d619d3b4a2ef738137f845f910fbb0
Your Name <your.email@example.com>
2025-09-01 18:39:28 +0300

    05 - Laravel Tutorial for Beginners #5 - Layouts & Slots

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,26 +1,15 @@
-<html lang="en">
+<x-layout>
+    <h2>Current Available Ninjas</h2>
+    <p>{{$greeting}}</p>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Blade Directive -->
 
-        <title>Ninjas Network | Home</title>
-    </head>
-
-    <body>
-        <h2>Current Available Ninjas</h2>
-        <p>{{$greeting}}</p>
-
-        <!-- Blade Directive -->
-
-        <ul>
-            @foreach($ninjas as $ninja)
-            <li>
-                <p>{{ $ninja['name'] }}</p>
-                <a href="/ninjas/{{ $ninja['id'] }}">View Detail</a>
-            </li>
-         @endforeach
-        </ul>
-    </body>
-
-</html>
+    <ul>
+        @foreach($ninjas as $ninja)
+        <li>
+            <p>{{ $ninja['name'] }}</p>
+            <a href="/ninjas/{{ $ninja['id'] }}">View Detail</a>
+        </li>
+        @endforeach
+    </ul>
+</x-layout>
