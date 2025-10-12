dc2f3976528ca4a9d765eba3a41bedde9d47dce9
Your Name <your.email@example.com>
2025-10-12 10:44:21 +0300

    card highlighting

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,9 @@
-<div class="card">
+@props(['highlight'=>false])
+
+<div @class(['highlight'=>$highlight, 'card'])>
+
     {{$slot}}
+
     <!-- passing href attributes  -->
     <!-- <a href="{{$attributes->get('href')}}" class="btn uppercase text-sm">View Details</a> -->
     <!-- easy way to pass href attribute -->
